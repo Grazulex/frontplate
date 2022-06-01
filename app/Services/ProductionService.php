25c9146f38efe364b2236567ead944f99b32950e
jean-marc strauven <jms@grazulex.be>
2022-06-01 14:30:28 +0200

    fix json issue

--- app/Services/ProductionService.php
+++ app/Services/ProductionService.php
@@ -18,8 +18,8 @@ class ProductionService {
         $fp = fopen('prod_'.$this->production->id.'.csv', 'w');
         fputcsv($fp, array('Plate nr.', 'Ref plaque', 'Order date', 'OwnerID', 'Nom Client', 'Rue Client', 'Nr. Client', 'Boite ClIENT', 'Commune', 'Code postal'), ";");
         foreach ($this->production->plates as $plate) {
-            $datas = json_decode($plate->datas);
-            fputcsv($fp, array($plate->reference, $plate->type, $plate->created_at, $plate->order_id, $plate->customer, $datas->destination_street, $datas->destination_house_number, $datas->destination_bus, $datas->destination_city, $datas->destination_postal_code), ";");
+            //$datas = $plate->datas;
+            fputcsv($fp, array($plate->reference, $plate->type, $plate->created_at, $plate->order_id, $plate->customer, $plate->datas['destination_street'], $plate->datas['destination_house_number'], $plate->datas['destination_bus'], $plate->datas['destination_city'], $plate->datas['destination_postal_code']), ";");
         }
         fclose($fp);
     }
